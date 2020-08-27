b5a63837d2d2df29b16c668e6b9d2a91435ea6cf
smallker <wahyu.smallker@gmail.com>
2020-08-27 18:52:19 +0700

    nambah raingauge

--- NodeMCU/README.md
+++ NodeMCU/README.md
@@ -1,3 +1,6 @@
 Bukake  nganggo visual studio code, tambah ekstensi platformio
 
 Tinggal klik File -> open folder
+
+Testing raingauge :
+Pin D5 tempelke ng GND, bar kui culke njur tempelke neh ben itungane nambah
--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 #include <FirebaseESP8266.h>
 #include <ESP8266WiFi.h>
-#define WIFI_SSID                 "MSP"
-#define WIFI_PASSWORD             "desmosedici"
-#define FIREBASE_HOST             "bisa-b2497.firebaseio.com"
-#define FIREBASE_AUTH             "RWiICJnjlpsVomTY9po6AYoAMYuWV4IhEgkMDSgE"
-#define FIREBASE_FCM_SERVER_KEY   "AAAAptP-Wxw:APA91bHMxI0A7eRx5t-odsfuyaEsh_PK7LZOfU0Kxhpf-s0Zl_kY4FlKnZ4p6Z10W8clyV9ONb_TavF062XJKak-5onri1wXhqb1tSbOov45TvoqTaGmLe91TRuXnRoGFUPWKU4Fyy9V"
-#define rainGauge                 D5
-#define trigPin                   D2
-#define echoPin                   D3
+#define WIFI_SSID "y"
+#define WIFI_PASSWORD "11111111"
+#define FIREBASE_HOST "bisa-b2497.firebaseio.com"
+#define FIREBASE_AUTH "RWiICJnjlpsVomTY9po6AYoAMYuWV4IhEgkMDSgE"
+#define FIREBASE_FCM_SERVER_KEY "AAAAptP-Wxw:APA91bHMxI0A7eRx5t-odsfuyaEsh_PK7LZOfU0Kxhpf-s0Zl_kY4FlKnZ4p6Z10W8clyV9ONb_TavF062XJKak-5onri1wXhqb1tSbOov45TvoqTaGmLe91TRuXnRoGFUPWKU4Fyy9V"
+#define rainGauge D5
+#define trigPin D2
+#define echoPin D3
 
 FirebaseData firebaseData;
 
+int rain;
+
 int ultrasonic(byte trigPin, byte echoPin);
+void ICACHE_RAM_ATTR rainGaugeSensor(void);
+
 void setup()
 {
   Serial.begin(9600);
-  pinMode(rainGauge,INPUT_PULLUP);
+  pinMode(rainGauge, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(rainGauge), rainGaugeSensor, FALLING);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -35,14 +40,15 @@ void setup()
 }
 void loop()
 {
-    FirebaseJson sensor;
-    int banjir = ultrasonic(trigPin,echoPin);
-    sensor.add("sensor", banjir);
-    Firebase.pushJSON(firebaseData, "/realtime/logs/", sensor);
-    delay(5000);
+  FirebaseJson sensor;
+  int banjir = ultrasonic(trigPin, echoPin);
+  sensor.add("sensor", banjir);
+  Firebase.pushJSON(firebaseData, "/realtime/logs/", sensor);
+  delay(5000);
 }
 
-int ultrasonic(byte trigPin,byte echoPin){
+int ultrasonic(byte trigPin, byte echoPin)
+{
   long duration;
   int distance;
   digitalWrite(trigPin, LOW);
@@ -51,7 +57,19 @@ int ultrasonic(byte trigPin,byte echoPin){
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance= duration*0.034/2;
-  Serial.println("Jarak => "+(String)distance);
+  distance = duration * 0.034 / 2;
+  Serial.println("Jarak => " + (String)distance);
   return distance;
+}
+
+void ICACHE_RAM_ATTR rainGaugeSensor()
+{
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > 200)
+  {
+    Serial.println("Hujan => " + (String)rain);
+    rain++;
+  }
+  last_interrupt_time = interrupt_time;
 }
